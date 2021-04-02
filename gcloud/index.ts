c513d8c78d40201ef2b645e23a9963bb795de7fc
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-02 18:50:39 +0700

    feat(gcloud): new gcp.appengine.Application()

--- gcloud/index.ts
+++ gcloud/index.ts
@@ -1,6 +1,15 @@
 import * as gke from "./gke";
 import { storageBucket as storage } from "./storage";
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+import { named } from "./config";
 
 export const clusterName = gke.cluster.name;
 export const clusterLocation = gke.cluster.location;
 export const storageBucket = storage.name;
+
+const app = new gcp.appengine.Application(named("app-engine"), {
+  locationId: "asia-east2",
+});
+
+export const appEngineName = app.name;
